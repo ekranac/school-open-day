2438ab44891367bc434eb7e58b52cd972c336c0a
ekranac <besal.ziga@gmail.com>
2015-02-08 12:44:17 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+function getTime(){
+    var d = new Date();
+
+    var h = d.getHours();
+    var m = d.getMinutes();
+
+    document.getElementById("time-h").innerHTML = h;
+    
+    if(m==0)
+    {
+        document.getElementById("time-m").innerHTML = m + "0";
+    }
+    else if(m <= 9)
+    {
+        document.getElementById("time-m").innerHTML = "0" + m;
+    }
+    else
+    {
+        document.getElementById("time-m").innerHTML = m;
+    }
+    
+    setTimeout(getTime, 1000);
+}
+
+
+$(document).ready(function(){
+    
+    var title = document.getElementById("myModalLabel");
+    var content = document.getElementById("myModalBody");
+    
+    $("#close-button").mouseenter(function(){
+        $("#close-button").click();
+    });
+    
+    $("#close-x").mouseenter(function(){
+        $("#close-x").click();
+    });
+    
+    
+    
+    $("#opis").mouseenter(function(){        
+        $("#opis").click();
+        title.innerHTML="Opis programov";
+        content.innerHTML="Opis programov...";
+    });
+    
+    $("#dejavnosti").mouseenter(function(){        
+        $("#dejavnosti").click();
+        title.innerHTML="Interesne dejavnosti";
+        content.innerHTML="Interesne dejavnosti...";
+    });
+    
+    $("#projektni").mouseenter(function(){
+        title.innerHTML="Projektni teden";
+        content.innerHTML="Projektni teden...";
+        $("#projektni").click();
+    });
+    
+    $("#praksa").mouseenter(function(){
+        title.innerHTML="Praksa";
+        content.innerHTML="Praksa...";
+        $("#praksa").click();
+    });
+    
+    $("#prihodnost").mouseenter(function(){
+        title.innerHTML="Prihodnost";
+        content.innerHTML="Prihodnost...";
+        $("#prihodnost").click();
+    });
+    
+    $("#predmetnik").mouseenter(function(){
+        title.innerHTML="Predmetnik";
+        content.innerHTML="Predmetnik...";
+        $("#predmetnik").click();
+    });
+    
+});
